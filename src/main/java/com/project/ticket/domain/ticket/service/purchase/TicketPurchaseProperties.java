8b4c29aa77862ec39c72985869a86e71963e622c
choonngg <a0107163@naver.com>
2026-05-09 14:55:51 +0900

    refactor: 구매 설정 접근자를 Lombok으로 전환

--- src/main/java/com/project/ticket/domain/ticket/service/purchase/TicketPurchaseProperties.java
+++ src/main/java/com/project/ticket/domain/ticket/service/purchase/TicketPurchaseProperties.java
@@ -1,18 +1,15 @@
 package com.project.ticket.domain.ticket.service.purchase;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+@Getter
+@Setter
 @Component
 @ConfigurationProperties(prefix = "ticket")
 public class TicketPurchaseProperties {
     private PurchaseStrategy purchaseStrategy = PurchaseStrategy.DISTRIBUTED;
 
-    public PurchaseStrategy getPurchaseStrategy() {
-        return purchaseStrategy;
-    }
-
-    public void setPurchaseStrategy(PurchaseStrategy purchaseStrategy) {
-        this.purchaseStrategy = purchaseStrategy;
-    }
 }
